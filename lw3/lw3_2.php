8dc3e04e00031dc8270d896fe542e86dd6b534d2
PsChester <Algaev18@gmail.com>
2020-03-07 02:39:40 +0300

    Дополнение к lw3
    Исправлена ошибка во втором задании lw3

--- lw3/lw3_2.php
+++ lw3/lw3_2.php
@@ -2,16 +2,16 @@
   header('Content-Type: text/plain');
   function getGETParameter(string $text): ? string
   {
-    return isset($_GET[$text]) ? (string)$_GET[$text] : null; //для чего string?
+    return isset($_GET[$text]) ? (string)$_GET[$text] : null;
   }
   
   $text = getGETParameter('identifier');
 
   if ($text !== null)
-    if ({хз какая команда}("/^[a-zA-Z]+[a-zA-Z0-9]*$/"))
-      echo 'No, invalid identifier';
-    else
+    if (preg_match("/^[a-zA-Z]+[a-zA-Z0-9]*$/", $text))
       echo 'Yes, correct identifier';
+    else
+      echo 'No, invalid identifier';
   else
     echo 'Not found identifier';
   
---
+++ lw3/lw3_4_jobs.php
@@ -0,0 +1,20 @@
+<?php
+  header('Content-Type: text/plain');
+  function GETParameter(string $text): ?string
+  {
+    return isset($_GET[$text]) ? (string)$_GET[$text] : null; //для чего string?
+  }
+  
+  $first_name = GETParameter('first_name');
+  $last_name = GETParameter('last_name');
+  $email = GETParameter('email');
+  $age = GETParameter('age');
+
+  $f_name1 = 'data/first_name.txt';
+  $f_name2 = 'data/last_name.txt';
+  $f_email = 'data/email.txt';
+  $f_age = 'data/age.txt';
+  file_put_contents($f_name1, $first_name);
+  file_put_contents($f_name2, $last_name);  
+  file_put_contents($f_email, $email);  
+  file_put_contents($f_age, $age);  
